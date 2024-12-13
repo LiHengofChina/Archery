38b8caac573915e067c72990aca502979e38207d
liheng_xiaomi <375461826@qq.com>
2024-12-13 14:08:02 +0800

    11

--- startup.sh
+++ startup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# 收集所有的静态文件到STATIC_ROOT
-python3 manage.py collectstatic -v0 --noinput
 
-# 启动服务
-supervisord -c supervisord.conf
+
+# 收集所有的静态文件到STATIC_ROOT
+source /opt/venv4archery/bin/activate
+cd /opt/archery
+python3 manage.py collectstatic --verbosity 2 --noinput
+
+
+# 使用 supervisord 来运行 gunicorn
+supervisord -c /opt/archery/supervisord.conf
+
+# 查看 supervisord的状态
+supervisorctl status
+
 
